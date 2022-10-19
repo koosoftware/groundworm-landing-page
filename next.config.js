d7b1e8ebca19a247e2b15e369f7004dac2b406c9
MC Koo <koomingchin@gmail.com>
2022-10-19 23:13:27 +0800

    set base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/about",
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
