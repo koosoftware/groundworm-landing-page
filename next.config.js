57e365f1e438d4379fa4ac76db3d0804fd206794
MC Koo <koomingchin@gmail.com>
2022-10-22 14:53:58 +0800

    revert

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/about",
-  i18n: {
-    locales: ["en", "ms"],
-    defaultLocale: "en",
-  },
   reactStrictMode: true,
   swcMinify: true,
 };
